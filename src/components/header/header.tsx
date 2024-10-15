b8d96c90d7e8ec24a6ef2d8cb999915839fdaf38
Luciano.Reis <lrr.arms233@gmail.com>
2024-10-15 20:03:57 -0300

    Organização e criação de nova TodoList em TS

---
+++ src/components/header/header.tsx
@@ -0,0 +1,11 @@
+export const Header: React.FC = () => {
+    return (
+        <header>
+            <div>
+                <h1>My Todo</h1>
+                
+                <span></span>
+            </div>
+        </header>   
+    )
+}
