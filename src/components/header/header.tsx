29e82edecdda2a9648dbbf6b1c5872666be0b9ab
Luciano.Reis <lrr.arms233@gmail.com>
2024-10-15 21:15:25 -0300

    Adicionada Estilização e Aplicabilidade

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,11 +1,23 @@
+import styles from "./styles.module.scss"
+import { StatsCard } from "../statsCard/StatsCard";
+
 export const Header: React.FC = () => {
     return (
-        <header>
-            <div>
-                <h1>My Todo</h1>
+        <header className={styles.header}>
+            <div className={styles.container}>
+                <div>
+                    <h1>My Todo</h1>
                 
-                <span></span>
+                    <span>Bem vindo, Luciano!</span>
+                </div>
+
+                <div>
+                    <StatsCard title="Total de Tarefas" value={5} />
+                    <StatsCard title="Tarefas Pendentes" value={4} />
+                    <StatsCard title="Tarefas Concluidas" value={1} />
+
+                </div>
             </div>
         </header>   
-    )
-}
+    );
+};
